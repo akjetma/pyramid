9e4b318073110bbe3459ea6bf3f569f017181fc1
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2016-01-25 18:13:01 -0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# pyramid
+
+Image LOD tiling
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject pyramid "0.1.0-SNAPSHOT"
+  :url "http://github.com/akjetma/pyramid"
+  :min-lein-version "2.5.3"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
