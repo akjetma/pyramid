80cb16040f92037ae8b7f5c018295ca24e3ef213
Adam Jetmalani <adamsinternetmailbox@gmail.com>
2016-01-27 00:09:15 -0800

    zoom-levels handler

--- src/pyramid/server.clj
+++ src/pyramid/server.clj
@@ -3,8 +3,46 @@
             [polaris.core :as polaris]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.util.response :refer [resource-response content-type]]
+            [ring.util.response :refer [response resource-response content-type]]
+            [clojure.data.json :as json]
+            [clojure.java.io :as io]
             [pyramid.util :as util]))
+
+
+;; --- Didn't feel this warranted a separate namespace ---
+
+(defn list-files
+  [path]
+  (remove 
+   #(.isHidden %)
+   (.listFiles (io/file (io/resource path)))))
+
+(def count-files (comp count list-files))
+
+(defn zoom-count
+  [zoom]
+  {:rows (count-files (util/zoom-path zoom))
+   :cols (count-files (util/row-path zoom 0))})
+
+(defn zoom-counts
+  []
+  (into 
+   {}
+   (map
+    (juxt str zoom-count)
+    (range 
+     (count-files util/tile-root)))))
+
+
+
+;; --- Server/handlers ---
+
+(defn zoom-levels-handler
+  [_]
+  (-> (zoom-counts)
+      json/write-str
+      response
+      (content-type "application/json")))
 
 (defn tile-handler
   [{tile :params}]
@@ -14,7 +52,8 @@
       (content-type "image/jpeg")))
 
 (def routes
-  [["/tile" :tile tile-handler]])
+  [["/tile" :tile tile-handler]
+   ["/zoom-levels" :zoom-levels zoom-levels-handler]])
 
 (def router
   (-> routes
--- src/pyramid/util.clj
+++ src/pyramid/util.clj
@@ -1,10 +1,32 @@
 (ns pyramid.util
-  (:require [clojure.stacktrace :as trace]))
+  (:require [clojure.string :as string]))
+
+(def resource-root
+  "/Users/adamjetmalani/code/pyramid/resources")
+
+(def tile-root 
+  "tile")
+
+(defn make-path
+  [& parts]
+  (string/join "/" parts))
 
 (defn resource-path
   [path]
-  (str "/Users/adamjetmalani/code/pyramid/resources/" path))
+  (make-path resource-root path))
+
+(defn zoom-path
+  [zoom]
+  (make-path tile-root zoom))
+
+(defn row-path
+  [zoom row]
+  (make-path 
+   (zoom-path zoom) 
+   row))
 
 (defn tile-path
   [{:keys [zoom row col]}]
-  (str "tile/" zoom "/" row "/" col ".jpg"))
+  (make-path 
+   (row-path zoom row)
+   (str col ".jpg")))
